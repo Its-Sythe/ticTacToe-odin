3e0f93318517a27e80189898463005e72c65fe6a
Its-Sythe <188770367+Its-Sythe@users.noreply.github.com>
2025-03-28 21:18:29 +0300

    Gameplay setting selection added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Game board
+
+
+// Gamestart
+const startGameBtn = document.getElementById("playGame-btn");
+startGameBtn.addEventListener("click", startGame)
+
+function startGame() {
+    const gameStyle = document.querySelector(".players-modal");
+
+    gameStyle.style.display = "flex";
+} 
